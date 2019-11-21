f2d7f0f3f6d1686b8f15b147fd614ad4f584bc63
Dominik Spicher <dominik.spicher@inacta.ch>
2019-11-21 09:03:14 +0100

    Updates

---
+++ precache-manifest.2e0d3b1caea4b7c1c0dd023c2a3db9f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc08c253247e30848a84641436db76b",
+    "url": "/swapping-sats/index.html"
+  },
+  {
+    "revision": "e88a83147323b13bb082",
+    "url": "/swapping-sats/static/css/main.de583310.chunk.css"
+  },
+  {
+    "revision": "a6fc72fa2160d768b786",
+    "url": "/swapping-sats/static/js/2.e08c2b4c.chunk.js"
+  },
+  {
+    "revision": "e88a83147323b13bb082",
+    "url": "/swapping-sats/static/js/main.d3a853f4.chunk.js"
+  },
+  {
+    "revision": "6fe815c2f703d725b1ce",
+    "url": "/swapping-sats/static/js/runtime-main.7b42ae07.js"
+  }
+]);
